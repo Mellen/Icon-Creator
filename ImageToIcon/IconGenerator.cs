3b60f76199c362df586dd88d0a64dc2870205ad2
Matt Ellen <Matt.Ellen@organox.com>
2022-09-15 12:34:07 +0100

    allow creating icon from multiple images

--- ImageToIcon/IconGenerator.cs
+++ ImageToIcon/IconGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -16,8 +17,20 @@ namespace ImageToIcon
             int bitmapByteCount = (int)msBitmap.Length;
             MemoryStream msIcon = new MemoryStream();
             msIcon.Write(new byte[] { 0, 0, 1, 0, 1, 0 }, 0, 6);
+
+            AddIconDirEntryToHeader(msIcon, image, 22);
+
+            msIcon.Write(msBitmap.ToArray(), 0, bitmapByteCount);
+
+            msBitmap.Close();
+
+            return msIcon;
+        }
+
+        private static ushort GetImageBitCount(Bitmap image)
+        {
             ushort bitCount = 32;
-            switch(image.PixelFormat)
+            switch (image.PixelFormat)
             {
                 case System.Drawing.Imaging.PixelFormat.Format16bppRgb565:
                 case System.Drawing.Imaging.PixelFormat.Format16bppRgb555:
@@ -36,19 +49,60 @@ namespace ImageToIcon
                     break;
             }
 
-            byte[] bitCountBytes = BitConverter.GetBytes(bitCount);
-            byte[] imageSizeBytes = BitConverter.GetBytes(bitmapByteCount);
+            return bitCount;
+        }
 
-            msIcon.Write(new byte[] { (byte)image.Width,
+        private static int AddIconDirEntryToHeader(MemoryStream stream, Bitmap image, int imageOffset)
+        {
+            using (MemoryStream msBitmap = new MemoryStream())
+            {
+                image.Save(msBitmap, System.Drawing.Imaging.ImageFormat.Png);
+
+                int bitmapByteCount = (int)msBitmap.Length;
+                ushort bitCount = GetImageBitCount(image);
+                byte[] bitCountBytes = BitConverter.GetBytes(bitCount);
+                byte[] imageSizeBytes = BitConverter.GetBytes(bitmapByteCount);
+                byte[] imageOffsetBytes = BitConverter.GetBytes(imageOffset);
+
+                stream.Write(new byte[] { (byte)image.Width,
                                       (byte)image.Height,
                                       0,
                                       0,
                                       0, 0}, 0, 6);
-            msIcon.Write(bitCountBytes, 0, bitCountBytes.Length);
-            msIcon.Write(imageSizeBytes, 0, imageSizeBytes.Length);
-            msIcon.Write(new byte[] { (byte)(6+6+bitCountBytes.Length+imageSizeBytes.Length+4),0,0,0,0 }, 0, 4);
+                stream.Write(bitCountBytes, 0, bitCountBytes.Length);
+                stream.Write(imageSizeBytes, 0, imageSizeBytes.Length);
+                stream.Write(imageOffsetBytes, 0, imageOffsetBytes.Length);
 
-            msIcon.Write(msBitmap.ToArray(), 0, bitmapByteCount);
+                imageOffset += bitmapByteCount;
+            }
+
+            return imageOffset;
+        }
+
+        public static MemoryStream GenerateIconStreamFromMultipleBitmaps(Bitmap[] images)
+        {
+            Debug.Assert(images.Length > 0);
+            Debug.Assert(images.Length < 8);
+            MemoryStream msIcon = new MemoryStream();
+            msIcon.Write(new byte[] { 0, 0, 1, 0, (byte)images.Length, 0 }, 0, 6);
+            int headerSize = 6 + 16*images.Length;
+            int imageOffset = headerSize;
+
+            foreach(Bitmap image in images)
+            {
+                imageOffset = AddIconDirEntryToHeader(msIcon, image, imageOffset);
+            }
+
+            foreach (Bitmap image in images)
+            {
+                using (MemoryStream msBitmap = new MemoryStream())
+                {
+                    image.Save(msBitmap, System.Drawing.Imaging.ImageFormat.Png);
+                    int bitmapByteCount = (int)msBitmap.Length;
+
+                    msIcon.Write(msBitmap.ToArray(), 0, bitmapByteCount);
+                }
+            }
 
             return msIcon;
         }
